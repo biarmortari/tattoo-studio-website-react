654b36781145b67fe8a45c634b88a0220453ffe6
biarmortari <beatrizrmortari@gmail.com>
2025-11-12 12:24:11 -0300

    feat: refatora o componente Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       <div className="page">
         <Header />
+        <Hero />
       </div>
     </>
   );
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero__content">
+        <h2 className="hero__title">SEJA BEM VINDO AO MEU MUNDO.</h2>
+      </div>
+    </section>
+  );
+}
