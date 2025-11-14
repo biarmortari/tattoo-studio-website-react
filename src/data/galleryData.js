0de4a0aa6d46bfdb73dd0b0c97bcf185130140d6
biarmortari <beatrizrmortari@gmail.com>
2025-11-14 12:02:05 -0300

    feat: cria o componente do catálogo e cria o arquivo data contendo o array com as imagens

---
+++ src/data/galleryData.js
@@ -0,0 +1,68 @@
+//trocar o src pelo caminho relativo
+
+export const galleryData = [
+  {
+    id: 1,
+    category: "mini-realismo",
+    src: catalogImages.catalog1,
+    title: "ALERT",
+    size: "8 a 12cm",
+    price: "R$ 800,00",
+  },
+  {
+    category: "mini-realismo",
+    image: catalogImages.catalog2,
+    title: "REAL EYES",
+    size: "Tamanho 10 a 13cm",
+    price: "R$ 850,00",
+  },
+  {
+    category: "mini-realismo",
+    image: catalogImages.catalog3,
+    title: "THE PIECES",
+    size: "Tamanho 8 a 10cm",
+    price: "R$ 750,00",
+  },
+  {
+    category: "mini-realismo",
+    image: catalogImages.catalog4,
+    title: "EXPLORER",
+    size: "Tamanho 10 a 12cm",
+    price: "R$ 800,00",
+  },
+  {
+    category: "mini-realismo",
+    image: catalogImages.catalog5,
+    title: "FLIGHT OF FREEDOM",
+    size: "Tamanho 12 a 15cm",
+    price: "R$ 850,00",
+  },
+  {
+    category: "mini-realismo",
+    image: catalogImages.catalog6,
+    title: "SMOKE MAN",
+    size: "Tamanho 10 a 15cm",
+    price: "R$ 850,00",
+  },
+  {
+    category: "mini-realismo",
+    image: catalogImages.catalog7,
+    title: "HEART SOUND",
+    size: "Tamanho 10 a 12cm",
+    price: "R$ 800,00",
+  },
+  {
+    category: "neo-tribal",
+    image: catalogImages.catalog8,
+    title: "BACKPIECE",
+    size: null,
+    price: "Valor cobrado por sessão",
+  },
+  {
+    category: "neo-tribal",
+    image: catalogImages.catalog9,
+    title: "BACKPIECE",
+    size: null,
+    price: "Valor cobrado por sessão",
+  },
+];
